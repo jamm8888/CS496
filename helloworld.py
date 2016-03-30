fd59453ef2bff5c1d026dcff28755bda129ed955
Jamm8888 <ammj@oregonstate.edu>
2016-03-29 21:13:00 -0700

    add logout to hellotest.py

--- helloworld.py
+++ helloworld.py
@@ -12,6 +12,7 @@ class MainPage(webapp2.RequestHandler):
 			if user:
 				self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
 				self.response.write('Hello You person, ' + user.nickname())
+				self.response.write(users.create_logout_url('/'))
 			else:
 				self.redirect(users.create_login_url(self.request.uri))
 
