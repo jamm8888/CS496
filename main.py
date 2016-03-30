5cbf793ea5c763275425c6ba5e5d2634b4b9a7ea
Jamm8888 <ammj@oregonstate.edu>
2016-03-30 12:15:52 -0700

    update to url strategy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from google.appengine.api import users
+from urlparse import urlparse
 
 import webapp2
 
@@ -8,9 +9,11 @@ class MainPage(webapp2.RequestHandler):
 		def get(self):
 			#Check if the user has an active account session
 			user = users.get_current_user()
+			url = urlparse(self.request.uri)
+			mainurl = url.scheme + "://" + url.netloc
 
 			if user:
-				newuri = ('%s/guestbook' % self.request.uri)
+				newuri = ('%s/guestbook' % mainurl)
 				self.redirect(newuri)
 			else:
 				self.redirect(users.create_login_url(self.request.uri))
