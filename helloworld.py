a5c3959976d3463c4491c1095f3045b1615cdf81
Jamm8888 <ammj@oregonstate.edu>
2016-03-29 21:05:55 -0700

    updates to helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,19 @@
+from google.appengine.api import users
+
 import webapp2
 
+
 class MainPage(webapp2.RequestHandler):
+
 		def get(self):
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.write('Hello, World!')
+			#Check if the user has an active account session
+			user = users.get_current_user()
+
+			if user:
+				self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+				self.response.write('Hello You person, ' + user.nickname())
+			else:
+				self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([
 	('/', MainPage),
